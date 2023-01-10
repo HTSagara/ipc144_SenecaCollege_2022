18087a4bb4da37d671095e1fff7142ad2083a6dd
HTSagara <95296571+HTSagara@users.noreply.github.com>
2023-01-10 12:48:17 -0500

    Add files via upload

---
+++ WS01/Workshop1/w1p1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+/*							Workshop - #1 (Part-1)>
+Full Name  : Henrique Toshio Sagara
+Student ID#: 170954218
+Email      : hsagara@myseneca.ca
+Section    : ZCC
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+
+*/
+int main() {
+	printf("Workshop 1 Part-1\n");
+	printf("=================\n");
+	printf("\n");
+	printf("I'm displaying this using the 'printf' stdio\n");
+	printf("(standard input output) library function!\n");
+	printf("\n");
+	printf("Dear teacher,\n");
+	printf("\n");
+	printf("  I promise I will work hard from this day onward.\n");
+	printf("  I acknowledge that practice is extremely important,\n");
+	printf("  so I will do all workshops, quizzes, and assignments.\n");
+	printf("\n");
+	printf("Sincerely,\n");
+	printf("\n");
+	printf("Henrique Sagara\n");
+	printf("Student ID# 170954218\n");
+	printf("\n");
+
+	return 0;
+}
